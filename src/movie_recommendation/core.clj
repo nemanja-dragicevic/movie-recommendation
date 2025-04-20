102ed0299248f50e7db05f290dd656a36d8ee2b7
Nemanja Dragicevic <nemanja543210@gmail.com>
2025-04-20 15:20:19 +0200

    Get user reviews and movies from V

--- src/movie_recommendation/core.clj
+++ src/movie_recommendation/core.clj
@@ -5,5 +5,26 @@
          (vec (for [_ (range cols)]
                 (rand scale))))))
 
-(initialize-feature-matrix 4 2 1)
+(defn print-matrix [matrix]
+  (doseq [row matrix]
+    (println row)))
+
+(def testR [[5 0 3]
+            [4 1 0]])
+(def testV [[1 0]
+            [0 1]
+            [1 1]])
+
+(defn fix-V-solve-U [R V] 
+  (for [row R]
+    (let [pairs (keep-indexed (fn [i v] (when (> v 0) [i v])) row)
+          indexes (mapv first pairs)
+          values (mapv second pairs)]
+      (do
+        (println "Row:" row, "Indexes:" indexes "Values:" values)
+        (let [temp-V (mapv #(nth V % 0) indexes)]
+          (println "Temp V:" temp-V)
+        )))))
+(fix-V-solve-U testR testV)
+
 
