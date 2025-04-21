066231f3bf2da96b24dd557e19d2947b69ef9596
Nemanja Dragicevic <nemanja543210@gmail.com>
2025-04-21 16:21:21 +0200

    Matrix multiplication check and identity matrix

--- src/movie_recommendation/core.clj
+++ src/movie_recommendation/core.clj
@@ -14,9 +14,33 @@
 (def testV [[1 0]
             [0 1]
             [1 1]])
+(def lambda 0.1)
 
 (defn transpose [matrix]
   (apply mapv vector matrix))
+
+(defn dot-product [v1 v2]
+  (reduce + (map * v1 v2)))
+
+(defn matrix-multiply
+  "Multiplying two matrices, where m2 is already transposed"
+  [m1 m2]
+  (let [cols-m1 (count (first m1))
+        rows-m2 (count (first m2))]
+    (if (= cols-m1 rows-m2)
+      (mapv (fn [row]
+              (mapv (fn [col] (dot-product row col)) m2))
+            m1)
+      (throw (IllegalArgumentException. (str "Matrices cannot be multiplied" rows-m2 cols-m1))))))
+(matrix-multiply testR (transpose testV))
+
+(defn identity-matrix [n lambda]
+  (mapv (fn [i]
+          (mapv (fn [j] (if (= i j) (+ 1 lambda) 0)) (range n)))
+        (range n)))
+(identity-matrix 3 lambda)
+
+(reduce + 0 [1 2 3 4]) 
 
 (defn fix-V-solve-U [R V] 
   (for [row R]
@@ -24,20 +48,16 @@
           indexes (mapv first pairs)
           values (mapv second pairs)]
       (do
-        (println "Row:" row, "Indexes:" indexes "Values:" values)
-        (let [temp-V (mapv #(nth V % 0) indexes)]
-          (println "Temp V:" temp-V, "Transposed V:" (transpose temp-V))
-        )))))
+        (let [temp-V (mapv #(nth V % 0) indexes)
+              result (matrix-multiply (transpose temp-V) (transpose temp-V))
+              id-mat (identity-matrix (count temp-V) lambda)]
+            (println "Result: " result, "ID-Matrix:" id-mat))))))
+        
 (fix-V-solve-U testR testV)
 
-(defn dot-product [v1 v2]
-  (reduce + (map * v1 v2)))
+(mapv (fn [x] x) (range 10)) 
 
-(defn matrix-multiply [m1 m2]
-  "Multiplying two matrices, where m2 is already transposed"
-  (mapv (fn [row]
-          (mapv (fn [col] (dot-product row col)) m2))
-        m1))
-(matrix-multiply testR (transpose testV))
+
+
 
 
