c9533e5fa74ca28fde34a36481c023364feabf4b
nemanja-dragicevic <nemanja543210@gmail.com>
2025-09-17 14:59:48 +0200

    Added lambda parameter to fix steps in ALS

--- src/movie_recommendation/core.clj
+++ src/movie_recommendation/core.clj
@@ -1,6 +1,7 @@
 (ns movie-recommendation.core
-  (:require [movie-recommendation.dataset :as dataset]
-            [clojure.core.matrix :as m]))
+  (:require
+   [movie-recommendation.dataset :as dataset]
+   [clojure.core.matrix :as m]))
 
 (m/set-current-implementation :vectorz)
 
@@ -17,6 +18,13 @@
 
 (defn transpose [matrix]
   (apply mapv vector matrix))
+
+;; (defn transpose [matrix odakle]
+;;   (println "To transpose:", matrix)
+;;   (println odakle)
+;;   (print-matrix matrix)
+;;   (println "----------------------")
+;;   (apply mapv vector matrix))
 
 (defn multiply-matrices [m1 m2]
   (try
@@ -37,16 +45,11 @@
       (str "Cannot add matrices with dimensions: ", "[", (count m1), " ", (count (first m1)), "]",
            "[", (count m2), " ", (count (first m2)), "]"))))
 
-(defn scale-matrix [matrix scalar]
-  (mapv (fn [row]
-          (mapv #(* % scalar) row))
-        matrix))
-
 (defn merge-into-matrix [v]
   (vec (for [row v]
          (vec (mapcat identity row)))))
 
-(defn fix-V-solve-U [R V]
+(defn fix-V-solve-U [R V lambda]
   (for [row R]
     (let [pairs (keep-indexed (fn [i v] (when (> v 0) [i v])) row)
           indexes (mapv first pairs)
@@ -65,7 +68,7 @@
       ;; (multiply-matrices (invert-2x2 (matrix-add result id-mat)) (transpose (multiply-matrices (transpose temp-V) values)))
       (multiply-matrices (m/inverse A) B))))
 
-(defn fix-U-solve-V [R U]
+(defn fix-U-solve-V [R U lambda]
   (for [col (transpose R)]
     (let [pairs (keep-indexed (fn [i v] (when (> v 0) [i v])) col)
           indexes (mapv first pairs)
@@ -75,39 +78,7 @@
           id-mat (identity-matrix (count result) lambda)
           A (add-matrices (multiply-matrices (transpose temp-U) temp-U) id-mat)
           B (multiply-matrices (transpose temp-U) (transpose values))]
-      ;; (println "Col: ", col)
-      ;; (println "Pairs: ", pairs, " Indexes: ", indexes, " Values: ", values, " id mat: ", id-mat)
-      ;; (println A)
-      ;; (println B)
       (multiply-matrices (m/inverse A) B))))
-
-(def new-U (merge-into-matrix (fix-V-solve-U testR testV)))
-new-U
-(def new-V (merge-into-matrix (fix-U-solve-V testR new-U)))
-new-V
-;; (merge-into-matrix (fix-V-solve-U testR testV))
-
-(multiply-matrices new-U (transpose new-V))
-
-
-(m/inverse [[0.6 -0.7] [-0.2 0.4]])
-
-(def A [[1 2 3]
-        [4 5 6]])
-
-(def B [[7 8]
-        [9 10]
-        [11 12]])
-
-(identity-matrix 3 lambda)
-(add-matrices B A)
-
-(multiply-matrices [[0.6 -0.7] [-0.2 0.4]] [[4.0 7.0] [2.0 6.0]])
-
-
-
-
-
 
 
 
